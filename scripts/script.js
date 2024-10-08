69bc7f315e6b2b3b309290580cb58a2bb45e8131
Eligio Elizarraraz <eligio2008@live.com.mx>
2024-10-08 12:51:47 -0600

    feat: added slide card template

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,20 @@
 const initialArticles = [
   {
     name: 'Laptop MovePad V5 22" 16GB 264GB Negro',
-    image: "../images/Laptop",
+    image: "../images/laptop.jpg",
     description:
       "Navega, explora y conéctate con confianza en esta Laptop. El MovePad V5 ofrece un rendimiento de alta capacidad de respuesta en un chasis delgado y compacto de 22 mm, lo que hace que la multitarea en movimiento sea muy sencilla. Aprovecha al máximo tu dispositivo con una pantalla sin marco para una mayor pantalla y parlantes, junto con 11 horas de duración de batería y carga rápida. Ideal para videollamadas, la cámara de 720P viene con un obturador de privacidad para mantener alejadas las miradas indiscretas, así como con cancelación inteligente de ruido para eliminar el ruido de fondo.",
+  },
+  {
+    name: "Auriculares FreeBuds 40H de Batería contra Polvo y Agua",
+    image: "./images/audifonos.jpg",
+    description:
+      "Auriculares de alto rendimiento totalmente inalámbricos. Hasta 9 horas de tiempo de escucha (más de 24 horas con estuche de carga) Ajustable; ganchos para los oídos de ajuste seguro para una comodidad y estabilidad ligeras. Diseño reforzado para la resistencia al sudor y al agua durante los entrenamientos difíciles.",
+  },
+  {
+    name: "Lentes Antirreflejantes con Filtro de Luz Azul Hombre",
+    image: "./images/lentes.jpg",
+    description:
+      "La lente de las gafas utiliza un filtro de luz azul, que bloquea la luz azul emitida cuando usa la computadora, el teléfono móvil, el iPad, el juego, etc. Muy adecuado para empleados de oficina o cualquier persona que necesite mirar la pantalla todo el día.",
   },
 ];
