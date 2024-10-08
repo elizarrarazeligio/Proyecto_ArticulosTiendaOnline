ab90d9170bd0094060bb5b67d9e50be0eab43d68
Eligio Elizarraraz <eligio2008@live.com.mx>
2024-10-07 22:36:24 -0600

    feat: added horizontal card template

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const initialArticles = [
+  {
+    name: 'Laptop MovePad V5 22" 16GB 264GB Negro',
+    image: "../images/Laptop",
+    description:
+      "Navega, explora y conéctate con confianza en esta Laptop. El MovePad V5 ofrece un rendimiento de alta capacidad de respuesta en un chasis delgado y compacto de 22 mm, lo que hace que la multitarea en movimiento sea muy sencilla. Aprovecha al máximo tu dispositivo con una pantalla sin marco para una mayor pantalla y parlantes, junto con 11 horas de duración de batería y carga rápida. Ideal para videollamadas, la cámara de 720P viene con un obturador de privacidad para mantener alejadas las miradas indiscretas, así como con cancelación inteligente de ruido para eliminar el ruido de fondo.",
+  },
+];
